999c91faf6434563874707fcc98b95b8da659624
Andrei 'Garoth' Thorp <garoth@gmail.com>
2008-11-19 11:16:09 -0500

    Made random sig thing work :D

--- scripts/makesig.sh
+++ scripts/makesig.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 # makes a signature using fortune
 
-/usr/bin/fortune /usr/share/fortune > $HOME/.signature-fortune
+/usr/bin/fortune 65% bofh-excuses 25% linux 10% /usr/share/fortune > $HOME/.signature-fortune
 cat $HOME/.signature-default $HOME/.signature-fortune
