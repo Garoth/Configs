4ddd516c04c621691a106d51e05378898b9f035e
Andrei Thorp <athorp@evernote.com>
2017-01-18 16:24:30 -0800

    comment about better to-gif conversion

--- scripts/convert-to-gif.sh
+++ scripts/convert-to-gif.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+# Some fancier options for scaling and stuff:
+# ffmpeg -i inccommand2.gif -filter:v "setpts=0.2*PTS"
+#        -vf scale=630:-1 incscale.gif 
+
 ffmpeg -i $1 -pix_fmt rgb24 tmp.gif
 convert -layers Optimize tmp.gif ${1%\.*}.gif
 rm tmp.gif
